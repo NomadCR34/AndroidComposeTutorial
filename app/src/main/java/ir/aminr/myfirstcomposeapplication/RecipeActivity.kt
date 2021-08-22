6718b7d3df9874b06d5c8716c868d39a509040c6
User <nomad.cr34@gmail.com>
2021-08-22 15:16:23 +0430

    added fragment,added compose usage with old views that not support with it

---
+++ app/src/main/java/ir/aminr/myfirstcomposeapplication/RecipeActivity.kt
@@ -0,0 +1,15 @@
+package ir.aminr.myfirstcomposeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_activity)
+        supportFragmentManager.beginTransaction().replace(
+            R.id.main_container,
+            RecipeListFragment()
+        ).commit()
+    }
+}

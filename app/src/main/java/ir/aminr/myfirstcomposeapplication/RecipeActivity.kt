6e5b234325baf08b7df7eb4f7101c809d4a07906
User <nomad.cr34@gmail.com>
2021-09-14 19:49:21 +0430

    added navigation

--- app/src/main/java/ir/aminr/myfirstcomposeapplication/RecipeActivity.kt
+++ app/src/main/java/ir/aminr/myfirstcomposeapplication/RecipeActivity.kt
@@ -2,6 +2,7 @@ package ir.aminr.myfirstcomposeapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

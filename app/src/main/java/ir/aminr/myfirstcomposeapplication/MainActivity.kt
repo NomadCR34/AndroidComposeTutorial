b5f273ff4e0dc8d7d3c505656a7fe266ca32824d
User <nomad.cr34@gmail.com>
2021-09-29 19:09:06 +0330

    Added test

--- app/src/main/java/ir/aminr/myfirstcomposeapplication/MainActivity.kt
+++ app/src/main/java/ir/aminr/myfirstcomposeapplication/MainActivity.kt
@@ -1,14 +1,11 @@
 package ir.aminr.myfirstcomposeapplication
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Space
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
@@ -23,12 +20,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ir.aminr.myfirstcomposeapplication.ui.theme.MyFirstComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

448697c99dc15b74f712919e563384e780c610dc
User <nomad.cr34@gmail.com>
2021-09-30 08:54:41 +0330

    commit 1

--- app/src/main/java/ir/aminr/myfirstcomposeapplication/MainActivity.kt
+++ app/src/main/java/ir/aminr/myfirstcomposeapplication/MainActivity.kt
@@ -4,89 +4,66 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            UserCard()
+            ContentView()
         }
     }
 }
 
 @Composable
-fun UserCard() {
-    val context = LocalContext.current
-    Row(
+fun ContentView() {
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(16.dp)
-            .border(1.dp, Color.Gray)
             .padding(8.dp)
+            .background(Color.Blue)
+            .fillMaxHeight(0.5f)
+            .width(300.dp)
+            .border(2.dp, Color.Magenta )
+            .padding(8.dp)
+            .border(5.dp, Color.White)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_user),
-            contentDescription = "user picture",
+        Text(
+            text = "Hello",
             modifier = Modifier
-                .size(96.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.Crop
+                .padding(8.dp),
+            color = colorResource(id = R.color.white)
         )
-        Column(
+
+        Spacer(modifier = Modifier.height(52.dp))
+
+        Text(
+            text = "World",
             modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Amin",
-                fontSize = 24.sp,
-                fontFamily = FontFamily.Monospace,
-                color = colorResource(id = R.color.purple_700)
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Button(onClick = {
-                Toast.makeText(
-                    context,
-                    "This is Amin info",
-                    Toast.LENGTH_LONG
-                ).show()
-            }) {
-                Text(text = "Show Info")
-            }
-        }
+                .offset(8.dp, 20.dp),
+            color = colorResource(id = R.color.white)
+        )
     }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        UserCard()
+    Surface {
+        ContentView()
     }
-
 }
